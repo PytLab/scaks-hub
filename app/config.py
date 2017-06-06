49c7696de1c07109e1f6940538f17f6bec762a92
pytlab <shaozhengjiang@gmail.com>
2017-06-06 19:25:28 +0800

    Add configure module.

---
+++ app/config.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+''' Configure for different apps.
+'''
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    ''' Base configure class.
+    '''
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'default secret key'
+    SQLALCHEMY_COMMINT_ON_TEARDOWN = True
+
+    @staticemethod
+    def init_app(app):
+        pass
+
+
+class DevConfig(Config):
+    ''' Configure class for development.
+    '''
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+
+class TestConfig(Config):
+    ''' Configure class for test.
+    '''
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+
+# All configure 
+config = {
+    'development': DevConfig,
+    'testing': TestConfig
+}
+
