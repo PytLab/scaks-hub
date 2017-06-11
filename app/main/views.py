9fe220c6d0744aa93e2525324911fa72ec11ff06
pytlab <shaozhengjiang@gmail.com>
2017-06-11 13:30:59 +0800

    添加网页icon

--- app/main/views.py
+++ app/main/views.py
@@ -3,7 +3,9 @@
 
 # View functions for microkinetics model building.
 
-from flask import render_template, url_for, redirect
+import os
+
+from flask import render_template, url_for, redirect, abort
 from . import main
 
 @main.route('/')
@@ -13,7 +15,11 @@ def index():
 @main.route('/tree/', defaults={'path': ''})
 @main.route('/tree/<path:path>', methods=['GET', 'POST'])
 def filetree(path):
+    base_path = os.getcwd()
     if path:
+        full_path = '{}/{}'.format(base_path, path)
+        if not os.path.exists(full_path):
+            abort(404)
         path = [subdir for subdir in path.split('/') if subdir]
     return render_template('filetree.html', path=path)
 
