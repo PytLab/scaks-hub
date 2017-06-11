a20c1812a31d99e03ad49df91f00844a90472f27
pytlab <shaozhengjiang@gmail.com>
2017-06-11 20:00:51 +0800

    添加文件列表显示

--- app/main/views.py
+++ app/main/views.py
@@ -18,10 +18,24 @@ def index():
 @main.route('/tree/<path:path>', methods=['GET', 'POST'])
 def filetree(path):
     base_path = os.getcwd()
+    full_path = '{}/{}'.format(base_path, path)
+
+    # Path information.
     if path:
-        full_path = '{}/{}'.format(base_path, path)
         if not os.path.exists(full_path):
             raise PathError('No such file or directory: {}'.format(full_path))
         path = [subdir for subdir in path.split('/') if subdir]
-    return render_template('filetree.html', path=path)
+
+        # Directory backward.
+        prev_path = '/'.join(path[: -1])
+
+    # File list.
+    dirs_files  = os.listdir(full_path)
+    dirs = [i for i in dirs_files if os.path.isdir('{}/{}'.format(full_path, i))]
+    files = [i for i in dirs_files if os.path.isfile('{}/{}'.format(full_path, i))]
+
+    return render_template('filetree.html',
+                           path=path,
+                           dirs=sorted(dirs),
+                           files=sorted(files))
 
