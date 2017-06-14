f60d2944452d4ef8cf693ea9b1c255526cdaeddf
pytlab <shaozhengjiang@gmail.com>
2017-06-14 23:31:26 +0800

    使用Ajax异步更新文件列表

---
+++ app/static/js/kynetix.js
@@ -0,0 +1,35 @@
+;(function($){
+    "use restrict";
+
+    // Refresh the file table asynchronously.
+    $("#refresh-file-table").click(function(){
+        // Show loading animation.
+        var ajax_load = '<div class="text-center">' +
+            '<img src="/static/img/broken-circle.gif" alg="Loading..."/></div>';
+        $("#file-table").html(ajax_load);
+
+        // Get new file table using ajax.
+        $.get(window.location.pathname, {}, function(data, textStatus){
+            var file_table_html = $(data).find("#file-table").html();
+            $("#file-table").html(file_table_html);
+
+            // Re-render time stamps using moment api.
+            moment.locale("en");
+            function flask_moment_render(elem) {
+                $(elem).text(eval('moment("' + $(elem).data('timestamp') + '").' + $(elem).data('format') + ';'));
+                $(elem).removeClass('flask-moment').show();
+            }
+            function flask_moment_render_all() {
+                $('.flask-moment').each(function() {
+                    flask_moment_render(this);
+                    if ($(this).data('refresh')) {
+                        (function(elem, interval) {
+                            setInterval(function() { flask_moment_render(elem) }, interval);
+                        })(this, $(this).data('refresh'));
+                    }
+                })
+            }
+        });
+    });
+})(jQuery);
+
