88eedfe10f9a12acb3c3818f4929bc5b089925eb
pytlab <shaozhengjiang@gmail.com>
2017-07-03 16:54:40 +0800

    使用gevent

--- manager.py
+++ manager.py
@@ -4,6 +4,10 @@ import os
 
 from flask_script import Manager, Shell
 
+from gevent import monkey
+from gevent.pywsgi import WSGIServer
+monkey.patch_all()
+
 from app import create_app
 
 app = create_app(os.getenv('KYN-CONFIG') or 'default') 
@@ -16,6 +20,20 @@ def test():
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+
+@manager.command
+def runserver(host='127.0.0.1', port=5000):
+    """ Run a gevent-based WSGIServer.
+    """
+    port = int(port)
+
+    server = WSGIServer(listener=(host, port), application=app)
+
+    def serve():
+        print(' * Running on http://{}:{}'.format(host, port))
+        server.serve_forever()
+
+    serve()
 
 def make_shell_context():
     return dict(app=app)
